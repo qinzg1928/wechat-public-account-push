9ee0217dbdf94adcbcc1854d6c5ca0662582688b
qinzg1928 <112927712+qinzg1928@users.noreply.github.com>
2022-09-06 15:10:05 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -43,6 +43,20 @@ export const config = {
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn"
     }, 
+      {
+      // 想要发送的人的名字
+      name: "🦌宝宝",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "omGSd5951iLkMihEGCPgqrmE0ye8",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "KUW_NjeD2fuejCEzQgFnnbQzj6136FNLBl0fuZC9SFk",
+      // 所在省份
+      province: "河北",
+      // 所在城市
+      city: "张家口",
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://wangxinleo.cn"
+    }, 
      ],
 
   /**
